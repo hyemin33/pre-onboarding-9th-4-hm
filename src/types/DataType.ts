3f24253c28eb8ec1548bbb883360a4cc22fcd9dc
hyemin33 <joliie@naver.com>
2023-03-20 16:00:04 +0900

    feat: data type 추가

---
+++ src/types/DataType.ts
@@ -0,0 +1,10 @@
+interface DataType {
+  id: number;
+  transaction_time: string;
+  status: boolean;
+  customer_id: number;
+  customer_name: string;
+  currency: string;
+}
+
+export default DataType;
